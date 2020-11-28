af13a9928d895adfb99edc763092e2021c006cf9
Mihajilo Mrsevic <mmrsevic98@gmail.com>
2020-11-28 18:51:27 +0100

    End of section 8

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -14,22 +14,33 @@ namespace API.Data
     {
         public static async Task SeedUsers(DataContext context)
         {
+            // fetch-uje sve korisnike
             if (await context.Users.AnyAsync()) return;
 
+            // cita sve iz JSON-a
             var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
+            // prebacuje u AppUser oblik
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+
             if (users == null) return;
+
             foreach(var user in users)
             {
+                // priprema random string za kriptovanje
                 using var hmac = new HMACSHA512();
 
+                // prebacuje username u mala slova
                 user.UserName = user.UserName.ToLower();
+                // priprema Hash od zadatakog stringa
                 user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
+                // priprema kljuc 
                 user.PasswordSalt = hmac.Key;
 
+                // dodaje korisnika
                await context.Users.AddAsync(user);
             }
 
+            // cuva u bazi
             await context.SaveChangesAsync();
         }
     }
