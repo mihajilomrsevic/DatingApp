3308182a571995e573f0579bd9abd64059057004
Mihajilo Mrsevic <mmrsevic98@gmail.com>
2020-11-29 03:09:58 +0100

    End of section 9

---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Member} from '../_models/member';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getMembers(): Observable<Member[]>{
+    return this.http.get<Member[]>(this.baseUrl + 'users');
+  }
+
+  getMember(username: string) {
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+}
